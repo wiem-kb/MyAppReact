0bfce739e016262f979c073832c44c941435c979
wiem-kb <wiemkbaier2024@gmail.com>
2025-11-17 23:57:50 +0100

    Update index.mjs

--- Server/src/index.mjs
+++ Server/src/index.mjs
@@ -17,8 +17,8 @@ app.get('/api/health', (req, res) => {
   res.json({ message: 'API is running' });
 });
 
-// Démarrer le serveur sur le port 5000
-const PORT = process.env.PORT || 5000;
+// Démarrer le serveur sur le port 9000
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
